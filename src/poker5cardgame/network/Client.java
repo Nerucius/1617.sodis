85b7a09b11e321402f551d7307be255bbdea8896
Herman Dempere <nerucius@gmail.com>
2017-03-05 01:25:56 +0100

    Added try/carch errors on Client and Server

--- src/poker5cardgame/network/Client.java
+++ src/poker5cardgame/network/Client.java
@@ -3,37 +3,64 @@ package poker5cardgame.network;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Scanner;
 
 public class Client {
 
-    ComUtils com;
-    
+    ComUtils com = null;
+
     public Client() {
-        
+
     }
-    
-    public void connect(String IP, int port){
+
+    public static void main(String... args) {
+        Scanner sc = new Scanner(System.in);
+        String line;
+        Client c = null;
+
+        while (!(line = sc.nextLine()).equals("q")) {
+            if (line.equals("connect")) {
+                c = new Client();
+                c.connect("localhost", 1212);
+            } else if (c != null) {
+                c.writeMsg(line);
+            }
+        }
+
+    }
+
+    public void connect(String IP, int port) {
         try {
             InetAddress address = InetAddress.getByName(IP);
             Socket sock = new Socket(address, port);
             com = new ComUtils(sock);
-            
+
         } catch (Exception ex) {
-            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Client: Failed to connect to Server on IP:" + IP + ".");
         }
-    
+
     }
-    
-    public void close(){        
+
+    public void close() {
+        if (com == null) {
+            System.err.println("Client: Not connected. Can't close()");
+            return;
+        }
+
+        try {
+            Socket sock = com.getSocket();
+            sock.close();
+            com = null;
+        } catch (IOException ex) {
+            System.err.println("Client: Error while closing Socket.");
+        }
     }
 
     public void writeMsg(String str) {
         try {
             com.write_string_variable(4, str);
         } catch (IOException ex) {
-            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Connection closed.");
         }
     }
 
--- src/poker5cardgame/network/Server.java
+++ src/poker5cardgame/network/Server.java
@@ -3,63 +3,90 @@ package poker5cardgame.network;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.net.SocketException;
+import poker5cardgame.game.NetworkSource;
 
-public class Server implements Runnable{
+public class Server implements Runnable {
 
-    ServerSocket serverSocket;
-    
+    ServerSocket serverSocket = null;
+
     public Server() {
-        
+
     }
-    
-    public void bind(int port){
+
+    public void bind(int port) {
+        if (serverSocket != null && serverSocket.isBound()) {
+            System.err.println("Server: Already bound to a port");
+            return;
+        }
+
         try {
             serverSocket = new ServerSocket(port);
         } catch (IOException ex) {
-            System.err.println("Fallo en crear socket");
+            System.err.println("Server: Failed to Open Socket.");
         }
     }
-    
-    public void start(){
-         new Thread(this).start();
+
+    public void start() {
+        if (serverSocket != null)
+            new Thread(this).start();
+        else
+            System.err.println("Server: Not bound to any port.");
     }
 
-     @Override
+    /**
+     * Main method to run while
+     */
+    @Override
     public void run() {
-        while (true) {
-            try {
-                Socket client = serverSocket.accept();
-                new Thread(new ServerRunnable(client)).start();
+        try {
 
-            } catch (IOException ex) {
-                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            // Main loop for accepting new Connections
+            while (true) {
+                Socket client = serverSocket.accept();
+                new Thread(new HandlerRunnable(client)).start();
             }
+
+        } catch (SocketException e) {
+            // Thrown when the server socket is closed();
+            System.err.println("Server: Closed");
+        } catch (IOException e) {
+            System.err.println("Server: Failed to accept(). Server Terminated");
         }
     }
 
     public void close() {
+        if (serverSocket == null) {
+            System.err.println("Server: Can't close server. Not started.");
+            return;
+        }
+
+        try {
+            serverSocket.close();
+            serverSocket = null;
+        } catch (Exception e) {
+        }
+
     }
-    
-    
-    private class ServerRunnable implements Runnable{
-        
+
+    private class HandlerRunnable implements Runnable {
+
+        NetworkSource source;
         ComUtils com;
-               
-        public ServerRunnable(Socket sock){
+
+        public HandlerRunnable(Socket sock) {
             com = new ComUtils(sock);
         }
-        
+
         @Override
         public void run() {
-            while(true){
-                try {
+            try {
+                while (true) {
                     String str = com.read_string_variable(4);
-                    System.out.println("Client says: " + str);
-                } catch (IOException ex) {
-                    System.err.println("Client Failure");
+                    System.out.println("echo: " + str);
                 }
+            } catch (IOException ex) {
+                System.err.println("Client Failure. Connection Closed");
             }
         }
     }
