23c5e39602de82382d87189e4ac8fcfd953cdc9f
SONIA BATLLORI PALLARES <sbatllpa9.alumnes@ub052229.mat.ub.edu>
2017-03-30 18:54:33 +0200

    Split Git into two projects
    Added Django Webapp Files

---
+++ P2 - Django/flylo/models.py
@@ -0,0 +1,20 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Flight(models.Model):
+
+    flight_number = models.CharField(max_length=8)
+    estimated_time_departure = models.DateTimeField()
+    estimated_time_arrival = models.DateTimeField()
+    location_departure = models.CharField(max_length=3)
+    location_arrival = models.CharField(max_length=3)
+    airline = models.CharField(max_length=3)
+    aircraft = models.CharField(max_length=4)
+    status = models.CharField(max_length=40)
+
+    def __str__(self):
+        return "[" + self.flight_number + "] " + self.location_departure + "-" + self.location_arrival + " : " + str(
+            self.estimated_time_departure) + " / " + str(self.estimated_time_arrival) + " [" + self.status + "]"
