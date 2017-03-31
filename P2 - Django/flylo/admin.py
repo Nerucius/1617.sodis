b1c3394e83eb88ca9b64efe128c0662b55e56cd8
Herman Dempere <nerucius@gmail.com>
2017-03-31 02:55:44 +0200

    Table display on Flight Admin
    Also removed and ignored some Python compile files

--- P2 - Django/flylo/admin.py
+++ P2 - Django/flylo/admin.py
@@ -4,7 +4,11 @@ from models import *
 # Register your models here.
 
 class FlightAdmin(admin.ModelAdmin):
-    pass
+    list_display = [f.name for f in Flight._meta.fields]
+
+    def __unicode__(self):
+        """ Function that returns the table display columns for Django-Admin. """
+        return self.list_display
 
 
 admin.site.register(Flight, FlightAdmin)
