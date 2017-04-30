f1e0ce7331a551ff17dd059a3088e65677ccac62
sbatllpa9 <sbatllpa9@alumnes.ub.edu>
2017-04-30 18:13:00 +0200

    Done Homework 1

--- P2 - Django/flylo/urls.py
+++ P2 - Django/flylo/urls.py
@@ -3,9 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	# home page
 	url(r'^$', views.index, name='index'),
-	# flight list
-	url(r'^flights/$', views.flights, name='flights'),
+	url(r'^flights/()$', views.flights, name='flights'),
 	url(r'^flights/(?P<departure>\w+)/$', views.flights, name='flights'),
 ]
--- P2 - Django/flylo/views.py
+++ P2 - Django/flylo/views.py
@@ -1,67 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from django.views.generic import ListView
-
-from .models import *
 from .forms import *
 
+# Flags to control which flight view has to be rendered
+SIMPLE_FLIGHTS = False
+DETAILED_FLIGHTS = True
+
+
 # Index view with a request, and a render response to index.html
-
-
 def index(request):
 	context = {}
 	# In case the form has been completed, redirect to the filtered by departure flights page
 	if request.method == 'POST':
 		form = SelectDepartureForm(request.POST)
 		if form.is_valid():
-			id = form.cleaned_data.get('departure')
-			departure = form.get_departure(id)
+			id_departure = form.cleaned_data.get('departure')
+			departure = form.get_departure(id_departure)
 			return HttpResponseRedirect('flights/' + departure)
 
 	# If not, send the empty form
 	else:
 		context['form'] = SelectDepartureForm()
-	return render(request, 'index.html', context)
+	return render(request, 'flylo/index.html', context)
 
 
-def flights(request, departure=None):
-	context = {}
-
-	if departure:
-		context['departure'] = departure
-		context['flights'] = Flight.objects.filter(location_departure=departure)
-
-	else:
-		context['flights'] = Flight.objects.all()
-
-	return render(request, 'flights.html', context)
-
-
-def simple_flight(request, pk=None):
-	context = {}
-	context['simple_flight'] = Flight.objects.values('flight_number', 'location_departure', 'location_arrival', 'status')
-
-	if pk:
-		context['simple_flight'] = context['simple_flights'].get(pk=pk)
-
-	return render(request, 'html', context)
-
-
-# TODO use
-# TODO add airplane info
-# TODO add airline info
-# This view returns a flight with all the available information, if pk is given,
-# or the list of all existing flights with all the available information, if not.
-'''def detailed_flights(request, pk=None):
-	context = {}
-	context['detailed_flights'] = Flight.objects.all()
-
-	if pk:
-		context['detailed_flights'] = context['detailed_flights'].get(pk=pk)
-
-	return render(request, 'html', context)'''
-
-'''
 def flights(request, departure=None):
 	context = {}
 
@@ -71,4 +33,11 @@ def flights(request, departure=None):
 	else:
 		context['flights'] = Flight.objects.all()
 
-	return render(request, 'flights.html', context'''
+	if SIMPLE_FLIGHTS:
+		template = 'flylo/simple_flights.html'
+	elif DETAILED_FLIGHTS:
+		template = 'flylo/detailed_flights.html'
+	else:
+		template = 'flylo/flights.html'
+
+	return render(request, template, context)
