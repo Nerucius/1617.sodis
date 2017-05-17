ac5365faa06acfe51b377863b23c76cec82ba7ff
sbatllpa9 <sbatllpa9@alumnes.ub.edu>
2017-05-17 08:58:52 +0200

    Building forms in checkin view template

--- P2 - Django/flylo/views.py
+++ P2 - Django/flylo/views.py
@@ -151,7 +151,7 @@ class ModifyCartView(View):
 
 				flight = Flight.objects.get(pk=fid)
 				airline = Airline.objects.get(code=airline)
-				price = flight.price * TYPE_MULT[type]
+				price = float(flight.price) * TYPE_MULT[type]  # TODO Note: sonia cast
 
 				# TODO Before creating reservations, check if flight has enough free seats, otherwise redirect to error page
 
